d583a4f8e19fd0694d58667cdbd65226fa9e8cbf
KZTahat <kztahat96@gmail.com>
2021-06-28 16:28:15 +0300

    adding josn file with map and onClick

--- src/Components/HornedBeasts.js
+++ src/Components/HornedBeasts.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
 
 class HornedBeasts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            numberOFvotes: 0
+        }
+    }
+
+    increaseVotes = () => {
+        this.setState({
+            numberOFvotes: this.state.numberOFvotes + 1
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.url} className='images' alt = {this.props.title} title={this.props.title}></img>
-                <p>{this.props.description}</p>
-            </div>
+            <>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img onClick={this.increaseVotes} variant="top" src={this.props.url} />
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                            {this.props.description}
+                        </Card.Text>
+                        <Card.Text>
+                            {this.state.numberOFvotes}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </>
         )
     }
 }
