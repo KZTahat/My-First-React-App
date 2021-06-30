e44a04aa8d330a401661610372dd2c6c89fa3642
KZTahat <kztahat96@gmail.com>
2021-06-30 15:58:38 +0300

    adding Filter Search

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,10 @@ import React from 'react';
 class Header extends React.Component{
     render(){
         return(
+            <>
             <h1>Hello there</h1>
+            <p> Welcom to my first React App </p>
+            </>
         )
     }
 }
